a8c9244481c5c3af35b7ea874144d57cbd2c0644
babumg1210 <babumg1210@outlook.com>
2025-03-23 21:06:47 +0530

    Update DemoApplication.java

--- javaapplication/demo/src/main/java/com/example/demo/DemoApplication.java
+++ javaapplication/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+@EnableZuulProxy
+public class ZuulServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
 }
